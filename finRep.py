711a4bf6e5045974a61b3a6fb8c377d680fcb1a1
katzbo <katz.boriso@gmail.com>
2019-07-23 22:46:24 +0300

    Add files via upload
    initial version

---
+++ finRep.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+import requests 
+
+googleApiKey = "AIzaSyAfrC2mRrLmbp0xqIPy5YZAw47e7QwCn3U"
+
+class GooglePlacesApiImpl:
+  	"""docstring for GooglePlacesApiImpl"""
+  	gSearchPlaceURL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json"
+	gGEtPlaceDetailsURL = "https://maps.googleapis.com/maps/api/place/details/json"
+
+  	def __init__(self, googleApiKey):
+  		self.googleApiKey = googleApiKey
+
+  	def getPlaceId(self, placeName):
+  		params = {'input':placeName,'inputtype':"textquery",'key':self.googleApiKey} 
+		data = getJsonResponse(self, gSearchPlaceURL, params)
+		try:
+			return data['candidates'][0]['place_id']
+		except:
+			raise ValueError("Place id request failed with json error response: " + data.text)
+
+	def getPlaceCategory(self, placeId):
+		params = {"key":googleApiKey,"placeid":placeId}  
+		#print(r.text)
+		data = getJsonResponse(self, gGEtPlaceDetailsURL, params)
+		try: 
+			placeCategory = data['result']['types']
+			print(placeCategory)
+		except:
+			raise ValueError("Category request failed with json error response: " + data.text)
+
+	def getJsonResponse(self, url, params):
+		r = requests.get(url = gGEtPlaceDetailsURL, params = params) 
+		return r.json()
+
+def addChargeToCategory(charge):
+	for category in categoryList:
+		if category.name == charge.category:
+			category.addCharge(charge)
+			print("added " + charge.name)
+			return
+	print("adding charge " + charge.name)
+	category = ChargeCatrgory(charge.category)
+	category.addCharge(charge)
+	print("adding category " + category.name)
+	categoryList.append(category)
+
+class Charge:
+  	def __init__(self, name ,price, companyName, date):
+	    self.name = name
+	    self.price = price
+	    self.companyName = companyName
+	    self.date = date
+
+
+class ChargeCatrgory:
+	"""docstring for ChargeCatrgory"""
+	def __init__(self, name):
+		self.name = name
+		self.sum = 0
+		self.charges = []
+
+	def addCharge(self, charge):
+		self.charges.append(charge)
+		self.sum+=charge.price
+		
+
+categoryList = []
+charge1 = Charge("delek", 12 ,"paz","12.2.84")
+addChargeToCategory(charge1)
+charge2 = Charge("delek", 34 ,"paz","12.2.84")
+addChargeToCategory(charge2)
+print("the sum of category " + categoryList[0].name + " and sum " + str(categoryList[0].sum))
+opts = Options()
+opts.set_headless()
+assert opts.headless  # Operating in headless mode
+print("financial report start")
+browser = webdriver.Firefox()
+browser.implicitly_wait(20)
+#browser.maximize_window()
+browser.get('https://www.leumi.co.il')
+login = browser.find_elements_by_class_name("includeLink")
+login[0].click()
+browser.find_element_by_id("uid").send_keys("boriso")
+browser.find_element_by_id("password").send_keys("katz111")
+table_id = browser.find_element_by_id('')
+rows = table_id.find_elements_by_tag("tr")
+
+for row in rows:
+	cols = row.find_elements_by_tag("td")
+	charge = Charge(cols[0], cols[1],cols[2],cols[3],cols[4])
+	addChargeToCategory(charge)
+
+
+
+
